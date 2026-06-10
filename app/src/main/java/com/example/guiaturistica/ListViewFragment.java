ac91b4565ff74e20d7ee3c4f57ed2e145f980b5b
Atlast19 <ahleandro18@gmail.com>
2026-06-10 09:10:33 -0400

    OK

--- app/src/main/java/com/example/guiaturistica/ListViewFragment.java
+++ app/src/main/java/com/example/guiaturistica/ListViewFragment.java
@@ -18,8 +18,7 @@ public class ListViewFragment extends Fragment {
 
     private ListView listView;
 
-    public ListViewFragment() {
-    }
+    public ListViewFragment() {}
 
     @Override
     public View onCreateView(
